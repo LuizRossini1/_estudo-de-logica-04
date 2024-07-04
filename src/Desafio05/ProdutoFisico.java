d7353ceb0761075dc3c75d4b0a003e43c9bd3513
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-04 19:24:30 -0300

    implementando lógica no mesmo método, classe 'ProdutoFisico'

--- src/Desafio05/ProdutoFisico.java
+++ src/Desafio05/ProdutoFisico.java
@@ -1,9 +1,20 @@
 package Desafio05;
 
+import java.util.function.Predicate;
+
 public class ProdutoFisico implements Calculavel {
     @Override
     public double calcularPrecoFinal(double valor) {
-        return 0;
+        double valorImposto = valor * taxaAdicional / 100;
+        double precoFinal = valor * (1 + taxaAdicional / 100);
+        return precoFinal;
+    }
 
+    public static void main(String[] args) {
+        ProdutoFisico mouse = new ProdutoFisico();
+        ProdutoFisico monitor = new ProdutoFisico();
+
+        System.out.println(mouse.calcularPrecoFinal(332.91));
+        System.out.println(monitor.calcularPrecoFinal(1059.99));
     }
 }
