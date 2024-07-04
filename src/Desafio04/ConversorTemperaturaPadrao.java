e0272d3c3d6922e7c673456c1104d52c5f100ae9
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-04 18:48:36 -0300

    implementando lógica método 'fahrenheitParaCelsius'

--- src/Desafio04/ConversorTemperaturaPadrao.java
+++ src/Desafio04/ConversorTemperaturaPadrao.java
@@ -13,15 +13,19 @@ public class ConversorTemperaturaPadrao implements ConversorTemperatura {
 
     @Override
     public double fahrenheitParaCelsius(double fahrenheit) {
-        return 0;
+        double resultado = (fahrenheit - 32) / 1.8;
+        return resultado;
 
     }
 
     public static void main(String[] args) {
         ConversorTemperaturaPadrao celParaFah = new ConversorTemperaturaPadrao();
+        ConversorTemperaturaPadrao fahParaCel = new ConversorTemperaturaPadrao();
 
         System.out.println(celParaFah.celsiusParaFahrenheit(15));
         System.out.println(celParaFah.celsiusParaFahrenheit(30));
 
+        System.out.println(fahParaCel.fahrenheitParaCelsius(59));
+        System.out.println(fahParaCel.fahrenheitParaCelsius(86));
     }
 }
