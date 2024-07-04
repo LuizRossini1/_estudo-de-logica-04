5367502ad5cae98f7a463841dc1608654220eeb5
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-04 20:01:01 -0300

    lógica de método

--- src/Desafio06/Servico.java
+++ src/Desafio06/Servico.java
@@ -1,13 +1,25 @@
 package Desafio06;
 
 public class Servico implements Vendavel {
+    protected double horasTrabalhadas = 8;
+
     @Override
     public double desconto(double valor) {
-        return 0;
+        double desconto = horasTrabalhadas * valor / 100;
+        return desconto;
     }
 
     @Override
     public double precoTotalPorQuantidade(double valor, double quantidade) {
-        return 0;
+        //Neste caso, a 'quantidade' será referente as horas trabalhadas
+        double precoHoras = valor * quantidade;
+        return precoHoras;
+    }
+
+    public static void main(String[] args) {
+        Servico exemplo1 = new Servico();
+
+        System.out.println(exemplo1.desconto(8.50));
+        System.out.println(exemplo1.precoTotalPorQuantidade(8.50, 8));
     }
 }
