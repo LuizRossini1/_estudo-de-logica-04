33b272b05dcc8cd9e35c6aaf179445338db2b720
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-04 18:02:44 -0300

    pacote, classe e interface desafio 3

---
+++ src/Desafio03/TabuadaMultiplicacao.java
@@ -0,0 +1,11 @@
+/* 3 - Crie uma classe TabuadaMultiplicacao que implementa uma interface Tabuada com o método mostrarTabuada() para
+exibir a tabuada de um número. A classe deve receber o número como parâmetro. */
+package Desafio03;
+
+public class TabuadaMultiplicacao implements Tabuada {
+    @Override
+    public int mostrarTabuada(int numero) {
+        return 0;
+    }
+
+}
